537aae5a0bf8f4bd0e4e6f530cb0399929830cc2
HiGamer-7 <geforcehigamer@gmail.com>
2026-09-23 19:14:27 +0400

    Update settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,1 +1,26 @@
 
+pluginManagement {
+    val flutterSdkPath = run {
+        val properties = java.util.Properties()
+        file("local.properties").let {
+            if (it.exists()) properties.load(it.inputStream())
+        }
+        properties.getProperty("flutter.sdk") ?: System.getenv("FLUTTER_ROOT")
+    }
+
+    includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
+
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+plugins {
+    id("dev.flutter.flutter-plugin-loader") version "1.0.0"
+    id("com.android.application") version "8.3.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+}
+
+include(":app")
